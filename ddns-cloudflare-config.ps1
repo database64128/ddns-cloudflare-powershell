2c1c697e730c0519b80212d4b23b0359e70410ec
database64128 <free122448@hotmail.com>
2021-02-20 14:59:10 +0800

    ⏱ Periodic DDNS updater
    - Separate code into functions
    - Use JSON as config
    - Periodic updater with custom interval

--- ddns-cloudflare-config.ps1
+++
@@ -1,7 +0,0 @@
-$LogFilePath = "/path/to/logfile.log"
-$CloudflareUriIPv6 = "https://api.cloudflare.com/client/v4/zones/<ZONE_ID>/dns_records/<RECORD_ID>"
-$CloudflareUriIPv4 = "https://api.cloudflare.com/client/v4/zones/<ZONE_ID>/dns_records/<RECORD_ID>"
-$Hostname = "domain.name"
-$OAuthToken = "<OAuthToken>"
-$EnableIPv6 = $True
-$EnableIPv4 = $True
