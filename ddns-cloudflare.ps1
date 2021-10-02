25bdd108680339984039bd5e3a8cad9edb341673
database64128 <free122448@hotmail.com>
2021-10-03 01:08:19 +0800

    🪵 Better logging + ConfigPath parameter

--- ddns-cloudflare.ps1
+++ ddns-cloudflare.ps1
@@ -4,6 +4,12 @@
 # Each time it checks the current IP against the DNS record to decide whether to create or update a DNS record.
 # This script supports both Linux and Windows.
 
+param (
+    [string]$ConfigPath = "settings.jsonc",
+    [switch]$Verbose,
+    [switch]$LogNoDate
+)
+
 # No macOS support.
 if ($IsMacOS) {
     Write-Host -Object "macOS is not supported."
@@ -14,12 +20,50 @@ function Write-Log {
     param (
         $Content
     )
-    $Content = "$(Get-Date -Format s) ${Content}"
+
+    if (!$LogNoDate) {
+        $Content = "$(Get-Date -Format s) $Content"
+    }
+
     if ($Settings.LogFilePath.Length -gt 0) {
         Out-File -InputObject $Content -FilePath $Settings.LogFilePath -Append
     }
     else {
         Write-Host -Object $Content
+    }
+}
+
+function Write-LogInformation {
+    param (
+        $Content
+    )
+
+    Write-Log -Content "[INFO] $Content"
+}
+
+function Write-LogWarning {
+    param (
+        $Content
+    )
+
+    Write-Log -Content "[WARN] $Content"
+}
+
+function Write-LogError {
+    param (
+        $Content
+    )
+
+    Write-Log -Content "[ERROR] $Content"
+}
+
+function Write-LogVerbose {
+    param (
+        $Content
+    )
+
+    if ($Verbose) {
+        Write-Log -Content "[DEBUG] $Content"
     }
 }
 
@@ -56,6 +100,8 @@ function New-DNSRecord {
 
     $Body = ConvertTo-Json $Data
 
+    Write-LogVerbose -Content "New-DNSRecord: Generated JSON payload $Body"
+
     return Invoke-RestMethod $DNSRecordsUri -Method Post -Authentication OAuth -Token $OAuthToken -Body $Body -ContentType "application/json" -TimeoutSec 15 -NoProxy:$Settings.NoProxy
 }
 
@@ -71,6 +117,8 @@ function Update-DNSRecordIP {
 
     $Body = ConvertTo-Json $Data
 
+    Write-LogVerbose -Content "Update-DNSRecordIP: Generated JSON payload $Body"
+
     return Invoke-RestMethod "$DNSRecordsUri/$RecordID" -Method Patch -Authentication OAuth -Token $OAuthToken -Body $Body -ContentType "application/json" -TimeoutSec 15 -NoProxy:$Settings.NoProxy
 }
 
@@ -81,15 +129,17 @@ function Update-IPv4DNSRecord {
         throw ("Failed to get current public IPv4 address: " + $Settings.IPv4.AddressAPI + " is probably down.");
     }
 
+    Write-LogVerbose -Content ("Update-IPv4DNSRecord: Got $IPv4 from " + $Settings.IPv4.AddressAPI)
+
     if ($null -eq $DNSRecordA) {
         # No existing A record. Create a new one.
         $Result = New-DNSRecord -Type "A" -Hostname $Settings.IPv4.Hostname -IP $IPv4 -TTL 60 -Proxied $Settings.IPv4.Proxied
         if ($Result.success) {
             $Script:DNSRecordA = $Result.result
-            Write-Log -Content ("Successfully created a new A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
+            Write-LogInformation -Content ("Successfully created a new A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
         }
         else {
-            Write-Log -Content ("Failed to create a new A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
+            Write-LogError -Content ("Failed to create a new A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
         }
     }
     else {
@@ -98,11 +148,14 @@ function Update-IPv4DNSRecord {
             $Result = Update-DNSRecordIP -RecordID $DNSRecordA.id -IP $IPv4
             if ($Result.success) {
                 $Script:DNSRecordA = $Result.result
-                Write-Log -Content ("Successfully updated A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
+                Write-LogInformation -Content ("Successfully updated A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
             }
             else {
-                Write-Log -Content ("Failed to update A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
+                Write-LogError -Content ("Failed to update A record for " + $Settings.IPv4.Hostname + " with IP ${IPv4}: $Result")
             }
+        }
+        else {
+            Write-LogVerbose -Content "Skipping identical IPv4 $IPv4"
         }
     }
 }
@@ -123,15 +176,17 @@ function Update-IPv6DNSRecord {
         throw "Failed to get current IPv6 address, skipping IPv6..."
     }
 
+    Write-LogVerbose -Content "Update-IPv6DNSRecord: Got $IPv6 from system"
+
     if ($null -eq $DNSRecordAAAA) {
         # No existing AAAA record. Create a new one.
         $Result = New-DNSRecord -Type "AAAA" -Hostname $Settings.IPv6.Hostname -IP $IPv6 -TTL 60 -Proxied $Settings.IPv6.Proxied
         if ($Result.success) {
             $Script:DNSRecordAAAA = $Result.result
-            Write-Log -Content ("Successfully created a new AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
+            Write-LogInformation -Content ("Successfully created a new AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
         }
         else {
-            Write-Log -Content ("Failed to create a new AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
+            Write-LogError -Content ("Failed to create a new AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
         }
     }
     else {
@@ -140,11 +195,14 @@ function Update-IPv6DNSRecord {
             $Result = Update-DNSRecordIP -RecordID $DNSRecordAAAA.id -IP $IPv6
             if ($Result.success) {
                 $Script:DNSRecordAAAA = $Result.result
-                Write-Log -Content ("Successfully updated AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
+                Write-LogInformation -Content ("Successfully updated AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
             }
             else {
-                Write-Log -Content ("Failed to update AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
+                Write-LogError -Content ("Failed to update AAAA record for " + $Settings.IPv6.Hostname + " with IP ${IPv6}: $Result")
             }
+        }
+        else {
+            Write-LogVerbose -Content "Skipping identical IPv6 $IPv6"
         }
     }
 }
@@ -154,20 +212,20 @@ function Initialize-DNSRecords {
         $Result = Get-DNSRecord -Hostname $Settings.IPv4.Hostname -Type "A"
         if ($Result.result.Length -gt 0) {
             $Script:DNSRecordA = $Result.result[0]
-            Write-Log -Content "Got initial A record: $DNSRecordA"
+            Write-LogInformation -Content "Got initial A record: $DNSRecordA"
         }
         else {
-            Write-Log -Content ("Found no existing A record for " + $Settings.IPv4.Hostname)
+            Write-LogInformation -Content ("Found no existing A record for " + $Settings.IPv4.Hostname)
         }
     }
     if ($Settings.IPv6.Enabled) {
         $Result = Get-DNSRecord -Hostname $Settings.IPv6.Hostname -Type "AAAA"
         if ($Result.result.Length -gt 0) {
             $Script:DNSRecordAAAA = $Result.result[0]
-            Write-Log -Content "Got initial AAAA record: $DNSRecordAAAA"
+            Write-LogInformation -Content "Got initial AAAA record: $DNSRecordAAAA"
         }
         else {
-            Write-Log -Content ("Found no existing AAAA record for " + $Settings.IPv6.Hostname)
+            Write-LogInformation -Content ("Found no existing AAAA record for " + $Settings.IPv6.Hostname)
         }
     }
 }
@@ -182,14 +240,14 @@ function Update-DNSRecords {
         }
     }
     catch {
-        Write-Log -Content "Exception thrown: $($_.Exception.Message)"
+        Write-LogError -Content "Exception thrown: $($_.Exception.Message)"
     }
 }
 
 $ErrorActionPreference = "Stop"
 
 # Load config
-$Settings = Get-Content -Path settings.jsonc | ConvertFrom-Json
+$Settings = Get-Content -Path $ConfigPath | ConvertFrom-Json
 $OAuthToken = ConvertTo-SecureString $Settings.OAuthToken -AsPlainText -Force
 $DNSRecordsUri = "https://api.cloudflare.com/client/v4/zones/" + $Settings.ZoneID + "/dns_records"
 
@@ -198,13 +256,13 @@ Initialize-DNSRecords
 
 # Start update loop
 if ($Settings.Interval -gt 0) {
-    Write-Log -Content "Started periodic DDNS updater at intervals of $($Settings.Interval) seconds."
+    Write-LogInformation -Content "Started periodic DDNS updater at intervals of $($Settings.Interval) seconds."
     while ($True) {
         Update-DNSRecords
         Start-Sleep -Seconds $Settings.Interval
     }
 }
 else {
-    Write-Log -Content "Started one-shot DDNS updater."
+    Write-LogInformation -Content "Started one-shot DDNS updater."
     Update-DNSRecords
 }
